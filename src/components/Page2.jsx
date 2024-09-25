b0d31b56e263eec5c0f359398211e76dcc7c5b01
Pranshu <thewebwizard12@gmail.com>
2024-09-26 03:27:59 +0530

    photo path change

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -3,7 +3,7 @@ const Page2 = () => {
     <div className="w-[90vw] min-h-[100vh] mx-auto ">
       <div className="flex flex-col md:flex-row justify-center items-center md:items-start gap-8">
         <img
-          src="src/assets/techfestphoto.jpg"
+          src="assets/techfestphoto.jpg"
           alt="Technorion Event"
           className="w-full md:w-1/2 h-auto mt-[3.4rem] rounded-lg shadow-lg"
         />
